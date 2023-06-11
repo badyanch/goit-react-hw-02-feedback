67bfd6766006bc2bcb797abcd14803bced06086f
Bohdan Zlotko <bohdanzlotko@gmail.com>
2023-06-11 18:25:14 +0300

    fix

--- src/components/Section/Section.styled.jsx
+++ src/components/Section/Section.styled.jsx
@@ -1,6 +1,6 @@
-import styled from 'styled-components';
+// import styled from 'styled-components';
 
-export const Title = styled.h2`
-  margin-bottom: 12px;
-  color: ${({ theme }) => theme.colors.primaryText};
-`;
+// export const Title = styled.h2`
+//   margin-bottom: 12px;
+//   color: ${({ theme }) => theme.colors.primaryText};
+// `;
